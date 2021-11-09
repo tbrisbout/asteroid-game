621f724e33cc78483872bdefffdd125451f4cfa8
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2021-11-09 22:04:24 +0100

    add falling gopher with hitbox

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"image/color"
 	_ "image/png"
 	"log"
 
@@ -11,20 +12,44 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-var img *ebiten.Image
+var (
+	img       *ebiten.Image
+	playerImg *ebiten.Image
 
-func init() {
-	var err error
-	img, _, err = ebitenutil.NewImageFromFile("gopher.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+	redColor   = color.RGBA{0xff, 0, 0, 0xff}
+	blueColor  = color.RGBA{0, 0, 0xdd, 0xff}
+	greenColor = color.RGBA{0, 0xff, 0, 0xaa}
+)
+
+const (
+	// welcomeMessage is intended to be used in DebugPrint function
+	welcomeMessage = `
+Press Enter to create a Gopher
+Press Escape to quit
+Use arrows (or hjkl) to move
+
+Press x to show hitboxes
+`
+
+	// startX is the initial x position of the falling gopher
+	startX = 200
+
+	// bottomThreshold is the line where image should disappear
+	bottomThreshold = 300
+
+	// imgWidth is the full width (including transparent) of the gopher image
+	imgWidth = 240
+)
 
 type Game struct {
+	fallingX, fallingY int
+	failedCount        int
+
 	gopherCount  int
 	x, y         int
 	mousePressed bool
+
+	showHitboxes bool
 }
 
 func (g *Game) Update() error {
@@ -38,19 +63,23 @@ func (g *Game) Update() error {
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyH) {
-		g.x--
+		g.x -= 3
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyL) {
-		g.x++
+		g.x += 3
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyK) {
-		g.y--
+		g.y -= 3
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyJ) {
-		g.y++
+		g.y += 3
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyX) {
+		g.showHitboxes = !g.showHitboxes
 	}
 
 	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
@@ -59,32 +88,70 @@ func (g *Game) Update() error {
 		g.mousePressed = false
 	}
 
+	if g.fallingY < bottomThreshold {
+		g.fallingY++
+	} else {
+		g.fallingY = 0
+		g.failedCount++
+	}
+
 	return nil
+}
+
+func overlap(a, b, wa, wb int) bool {
+	return a < b+wb && b < a+wa
+}
+
+func (g *Game) hit() bool {
+	return overlap(g.x, g.fallingX, imgWidth, imgWidth) && overlap(g.y, g.fallingY, imgWidth, imgWidth)
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	if g.gopherCount == 0 {
-		ebitenutil.DebugPrint(screen, `
-Press Enter to create a Gopher
-Press Escape to quit
-Use arrows (or hjkl) to move
-`)
+		ebitenutil.DebugPrint(screen, welcomeMessage)
 		return
 	}
 
+	// draw player gopher
 	for i := 0; i < g.gopherCount; i++ {
 		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(50*i+g.x*3), float64(g.y*3))
+		op.GeoM.Translate(float64(50*i+g.x), float64(g.y))
 
 		if g.mousePressed {
 			op.GeoM.Scale(1.5, 1)
 		}
 
-		screen.DrawImage(img, op)
+		if g.showHitboxes {
+			playerImgHB := ebiten.NewImageFromImage(img)
+			playerImgHB.Fill(redColor)
+			screen.DrawImage(playerImgHB, op)
+		}
+
+		screen.DrawImage(playerImg, op)
 	}
 
-	mx, my := ebiten.CursorPosition()
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("cursor position: %d, %d", mx, my))
+	// draw falling gopher
+	if g.fallingY < bottomThreshold {
+		fallingOp := &ebiten.DrawImageOptions{}
+		fallingOp.GeoM.Translate(float64(g.fallingX), float64(g.fallingY))
+
+		if g.hit() {
+			if g.showHitboxes {
+				imgHB := ebiten.NewImageFromImage(img)
+				imgHB.Fill(greenColor)
+				screen.DrawImage(imgHB, fallingOp)
+			}
+		} else {
+			if g.showHitboxes {
+				imgHB := ebiten.NewImageFromImage(img)
+				imgHB.Fill(blueColor)
+				screen.DrawImage(imgHB, fallingOp)
+			}
+		}
+		screen.DrawImage(img, fallingOp)
+	}
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("failed: %d", g.failedCount))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -92,9 +159,21 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	playerImg, _, err = ebitenutil.NewImageFromFile("gopher.png")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, Matrix WoRlD!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowTitle("Hello, Hitbox WoRlD!")
+	if err := ebiten.RunGame(&Game{fallingX: startX}); err != nil {
 		log.Fatal(err)
 	}
 }
